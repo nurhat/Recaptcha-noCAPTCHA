15e42d2b005ddfe3761d2419adadf0a022a2e677
nurhat.uca@gmail.com <nurhat.uca@gmail.com>
2014-12-05 09:14:16 +0200

    missing files

---
+++ MvcGooogleNoCaptcha/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MvcGooogleNoCaptcha.Infrastructure.Recaptcha;
+using MvcGooogleNoCaptcha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcGooogleNoCaptcha.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RecaptchaValidationActionFilter]
+        [HttpPost]
+        public ActionResult Index(IndexModel model, bool recaptchaValid, string recaptchaErrorMessage)
+        {
+            if (!ViewData.ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            else if (!recaptchaValid)
+            {
+                ViewData.ModelState.AddModelError("_recaptchaError", recaptchaErrorMessage);
+                return View("Index", model);
+            }
+            return View("Welcome", model);
+        }
+        
+
+    }
+}
